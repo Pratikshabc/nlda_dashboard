65df2d8931131092e7e47998a2c0923b2b3d0d3a
ChandraSai1702 <chandrasai@sigmoidanalytics.com>
2025-02-07 12:44:15 +0530

    Saved local changes before pull

--- nlda_dashboard/src/App.jsx
+++ nlda_dashboard/src/App.jsx
@@ -34,8 +34,6 @@
 
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { Drawer, AppBar, Toolbar, IconButton, List, ListItem, ListItemText, Button } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
 import Chatbot from './components/Chatbot';
 import SavedQueries from './components/SavedQueries';
 import Visualization from './components/Visualization';
@@ -49,32 +47,16 @@ const App = () => {
 
   return (
     <Router>
-      <AppBar position="sticky" color="primary">
-        <Toolbar>
-          <IconButton edge="start" color="inherit" onClick={() => toggleDrawer(true)} aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Button color="inherit" component={Link} to="/" className="ml-auto">
-            Home
-          </Button>
-        </Toolbar>
-      </AppBar>
-
-      {/* Drawer (Hamburger Menu) */}
-      <Drawer
-        anchor="left"
-        open={open}
-        onClose={() => toggleDrawer(false)}
-      >
-        <List>
-          <ListItem button onClick={() => toggleDrawer(false)} component={Link} to="/">
-            <ListItemText primary="Chatbot" />
-          </ListItem>
-          <ListItem button onClick={() => toggleDrawer(false)} component={Link} to="/saved-queries">
-            <ListItemText primary="Saved Queries" />
-          </ListItem>
-        </List>
-      </Drawer>
+      <nav className="p-4 bg-blue-600 text-white">
+         <ul className="flex space-x-6">
+           <li>
+             <Link to="/" className="hover:underline">Chatbot</Link>
+           </li>
+           <li>
+             <Link to="/saved-queries" className="hover:underline">Saved Queries</Link>
+           </li>
+         </ul>
+       </nav>
 
       <Routes>
         <Route path="/" element={<Chatbot />} />
